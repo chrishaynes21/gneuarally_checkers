6a98b52267ee5744c7592c5b47ad0440910ccc4e
rmwillia <ryanwilliams100@gmail.com>
2018-12-11 15:05:25 -0700

    updated mains

--- minimax.py
+++ minimax.py
@@ -87,77 +87,77 @@ def minimax(board, depthLeft, alpha, beta, isMax=True):
 
         return bestValue, bestMove
 
-if __name__ == '__main__':
-    from board import Board
-    from piece import Color, Piece
-    import numpy as np
-
-    # Setup
-    board = Board()
-    board.printState()
-
-    # Game 1
-    piece = Piece(Color.RED, [3, 2])
-    piece2 = Piece(Color.BLACK, [2, 1])
-    draught = np.empty(shape=(8, 8), dtype=object)
-    draught[3, 2] = piece
-    draught[2, 1] = piece2
-    board.setBoard(draught)
-    board.printState()
-
-    value, move = minimax(board, 9, -inf, inf)
-    board.makeMove(move)
-    print(value)
-    board.printState()
-
-    # # Game 2
-    # piece = Piece(Color.RED, [3, 4])
-    # piece4 = Piece(Color.RED, [3, 2])
-    # piece5 = Piece(Color.RED, [2, 5])
-    # piece2 = Piece(Color.BLACK, [2, 3])
-    # piece3 = Piece(Color.BLACK, [0, 3])
-    # draught = np.empty(shape=(8, 8), dtype=object)
-    # draught[3, 4] = piece
-    # draught[3, 2] = piece4
-    # draught[2, 5] = piece5
-    # draught[2, 3] = piece2
-    # draught[0, 3] = piece3
-    # board.setBoard(draught)
-    # board.printState()
-    #
-    # isMax = True
-    # isOver, _ = board.isOver()
-    # while not isOver:
-    #     value, move = minimax(board, 10, -inf, inf, isMax)
-    #     print("move: ", move)
-    #     if move is None:
-    #         print('move is None. Stopping')
-    #         break
-    #     print("\nPlayer", board.turn, "to", move, "for value", value)
-    #     board.makeMove(move)
-    #     print(board)
-    #     isMax = not isMax
-    #     isOver, _ = board.isOver()
-
-    # # Game 3
-    # board = Board()
-    # board.printState()
-    #
-    # isMax = True
-    # isOver, _ = board.isOver()
-    # while not isOver:
-    #     if board.turn == Color.BLACK:
-    #         move = board.validMoves()[int(len(board.validMoves()) / 2)]
-    #         print("\nPlayer", board.turn, "to", move)
-    #         board.makeMove(move)
-    #     else:
-    #         value, move = minimax(board, 10, -inf, inf, isMax)
-    #         print("move: ", move)
-    #         if move is None:
-    #             print('move is None. Stopping')
-    #             break
-    #         print("\nPlayer", board.turn, "to", move, "for value", value)
-    #         board.makeMove(move)
-    #     print(board)
-    #     isMax = not isMax
-    #     isOver, _ = board.isOver()
+# if __name__ == '__main__':
+#     from board import Board
+#     from piece import Color, Piece
+#     import numpy as np
+#
+#     # Setup
+#     board = Board()
+#     board.printState()
+#
+#     # Game 1
+#     piece = Piece(Color.RED, [3, 2])
+#     piece2 = Piece(Color.BLACK, [2, 1])
+#     draught = np.empty(shape=(8, 8), dtype=object)
+#     draught[3, 2] = piece
+#     draught[2, 1] = piece2
+#     board.setBoard(draught)
+#     board.printState()
+#
+#     value, move = minimax(board, 9, -inf, inf)
+#     board.makeMove(move)
+#     print(value)
+#     board.printState()
+#
+#     # Game 2
+#     piece = Piece(Color.RED, [3, 4])
+#     piece4 = Piece(Color.RED, [3, 2])
+#     piece5 = Piece(Color.RED, [2, 5])
+#     piece2 = Piece(Color.BLACK, [2, 3])
+#     piece3 = Piece(Color.BLACK, [0, 3])
+#     draught = np.empty(shape=(8, 8), dtype=object)
+#     draught[3, 4] = piece
+#     draught[3, 2] = piece4
+#     draught[2, 5] = piece5
+#     draught[2, 3] = piece2
+#     draught[0, 3] = piece3
+#     board.setBoard(draught)
+#     board.printState()
+#
+#     isMax = True
+#     isOver, _ = board.isOver()
+#     while not isOver:
+#         value, move = minimax(board, 10, -inf, inf, isMax)
+#         print("move: ", move)
+#         if move is None:
+#             print('move is None. Stopping')
+#             break
+#         print("\nPlayer", board.turn, "to", move, "for value", value)
+#         board.makeMove(move)
+#         print(board)
+#         isMax = not isMax
+#         isOver, _ = board.isOver()
+#
+#     # Game 3
+#     board = Board()
+#     board.printState()
+#
+#     isMax = True
+#     isOver, _ = board.isOver()
+#     while not isOver:
+#         if board.turn == Color.BLACK:
+#             move = board.validMoves()[int(len(board.validMoves()) / 2)]
+#             print("\nPlayer", board.turn, "to", move)
+#             board.makeMove(move)
+#         else:
+#             value, move = minimax(board, 10, -inf, inf, isMax)
+#             print("move: ", move)
+#             if move is None:
+#                 print('move is None. Stopping')
+#                 break
+#             print("\nPlayer", board.turn, "to", move, "for value", value)
+#             board.makeMove(move)
+#         print(board)
+#         isMax = not isMax
+#         isOver, _ = board.isOver()
